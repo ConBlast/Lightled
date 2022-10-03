1647306a585b669fe028b88476461a3ebda57952
Christian Castro <85885192+ConBlast@users.noreply.github.com>
2022-10-03 16:04:52 -0500

    Led

--- arduino-serial-com/arduino-serial-com.ino
+++ arduino-serial-com/arduino-serial-com.ino
@@ -1,101 +1,103 @@
-/*
-Peripherals:
- - Potentiometer
- - Push-button switch
- - Buzzer
-*/
+//Leds posiciones
+const int LED1 = 6;
+const int LED2 = 8;
+const int LED3 = 3;
+const int LED4 = 1;
+const int LED5 = 5;
+const int LED6 = 4;
+const int LED7 = 2;
 
-const int POTEN_PIN = A5;
-const int BTN_A_PIN = 2;
-const int BTN_B_PIN = 3;
-const int PIEZO_BUZZ_PIN = 6;
+const int sensor = 12;
+const int echo = 13;
 
-bool btnAState = false;
-bool isAPressed = false;
-
-bool btnBState = false;
-bool isBPressed = false;
-
-int potentiometerValue = 0;
-int previousPotenValue = potentiometerValue;
-
+int duracion = 0;
+int distancia = 0;
 
 void setup() {
-  pinMode(POTEN_PIN, INPUT);
-  pinMode(BTN_A_PIN, INPUT);
-  pinMode(BTN_B_PIN, INPUT);
-  pinMode(PIEZO_BUZZ_PIN, OUTPUT);
 
   Serial.begin(9600);
+
+  pinMode(sensor , OUTPUT);
+  pinMode(echo , INPUT);
+  
+  //Led output
+  pinMode(LED1 , OUTPUT);
+  pinMode(LED2 , OUTPUT);
+  pinMode(LED3 , OUTPUT);
+  pinMode(LED4 , OUTPUT);
+  pinMode(LED5 , OUTPUT);
+  pinMode(LED6 , OUTPUT);
+  pinMode(LED7 , OUTPUT);
+  
 }
 
 void loop() {
 
-  if (Serial.available() > 0) {
-    receivingData();
-  } else {
-    sendingData();
+
+  digitalWrite(sensor , HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trig , LOW);
+
+
+  duration = pulseIn(sensor , HIGH);
+  distance = (duracion/2) / 28.5 ;
+  Serial.println(distancia);
+  
+  //High encendido, low apagado, distancia a la que lo lee y dependiendo de eso lo prende o lo apaga
+  if ( distance <= 3 )
+  {
+    digitalWrite(LED1, HIGH);
   }
-  delay(10);
-}
-
-void sendingData() {
-
-  potentiometerValue = analogRead(POTEN_PIN);
-  btnAState = digitalRead(BTN_A_PIN);
-  btnBState = digitalRead(BTN_B_PIN);
-
-  if (previousPotenValue != potentiometerValue) {
-    sendSerialMessage('O', 'O', potentiometerValue);
-    previousPotenValue = potentiometerValue;
+  else
+  {
+    digitalWrite(LED1, LOW);
   }
-
-  if (btnAState && !isAPressed && !btnBState) {
-    sendSerialMessage('W', '0', potentiometerValue);
-    isAPressed = true;
-  } else if (!btnAState && isAPressed) {
-    isAPressed = false;
+  if ( distance <= 7 )
+  {
+    digitalWrite(LED2, HIGH);
   }
-
-  if (btnBState && !isBPressed && !btnAState) {
-    sendSerialMessage('O', 'S', potentiometerValue);
-    isBPressed = true;
-  } else if (!btnBState && isBPressed) {
-    isBPressed = false;
+  else
+  {
+    digitalWrite(LED2, LOW);
   }
-
-  delay(100);
-}
-
-void receivingData() {
-
-  char inByte = Serial.read();
-
-  switch (inByte) {
-    case 'P':
-      tone(PIEZO_BUZZ_PIN, 500);
-      delay(100);
-      tone(PIEZO_BUZZ_PIN, 1500);
-      delay(500);
-      noTone(PIEZO_BUZZ_PIN);
-      break;
-    case 'N':
-      tone(PIEZO_BUZZ_PIN, 300);
-      delay(400);
-      tone(PIEZO_BUZZ_PIN, 250);
-      delay(200);
-      noTone(PIEZO_BUZZ_PIN);
-      break;
+  if ( distance <= 10 )
+  {
+    digitalWrite(LED3, HIGH);
   }
-  Serial.flush();
-}
-
-void sendSerialMessage(char keyA, char keyB, int potenValue) {
-  Serial.print(keyA);
-  Serial.print(' ');
-  Serial.print(keyB);
-  Serial.print(' ');
-  Serial.print(potenValue);
-  Serial.print(' ');
-  Serial.println();
+  else
+  {
+    digitalWrite(LED3, LOW);
+  }
+  if ( distance <= 18 )
+  {
+    digitalWrite(LED4, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED4, LOW);
+  }
+  if ( distance <= 23 )
+  {
+    digitalWrite(LED5, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED5, LOW);
+  }
+  if ( distance <= 28 )
+  {
+    digitalWrite(LED6, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED6, LOW);
+  }
+  if ( distance <= 31 )
+  {
+    digitalWrite(LED7, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED7, LOW);
+  }
 }
